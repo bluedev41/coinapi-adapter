57b1ff0bc4c6678db4156a356aae8b8ba26e6252
Yu-Dev <lollanoyer@gmail.com>
2019-11-19 13:20:32 -0600

    Update adapter to use result key

--- index.js
+++ index.js
@@ -1,57 +1,59 @@
-const request = require('request');
+const request = require('request')
 
 const createRequest = (input, callback) => {
-	let url = "https://rest.coinapi.io/v1/exchangerate/";
-	const coin = input.data.coin || "ETH";
-	const market = input.data.market || "USD";
-	url = url + coin + "/" + market
-	let options = {
-		url: url,
-		qs: {
-			apikey: process.env.API_KEY
-		},
-		json: true
-	}
-	request(options, (error, response, body) => {
-		if (error || response.statusCode >= 400 || body.error) {
-			callback(response.statusCode, {
-				jobRunID: input.id,
-				status: "errored",
-				error: body,
-				errorMessage: body.error,
-				statusCode: response.statusCode
-			});
-		} else {
-			callback(response.statusCode, {
-				jobRunID: input.id,
-				data: body,
-				result: body.rate,
-				statusCode: response.statusCode
-			});
-		}
-	});
-};
+  let url = 'https://rest.coinapi.io/v1/exchangerate/'
+  const coin = input.data.coin || 'ETH'
+  const market = input.data.market || 'USD'
+  url = url + coin + '/' + market
+  let options = {
+    url: url,
+    qs: {
+      apikey: process.env.API_KEY
+    },
+    json: true
+  }
+  request(options, (error, response, body) => {
+    if (error || response.statusCode >= 400 || body.error) {
+      callback(response.statusCode, {
+        jobRunID: input.id,
+        status: 'errored',
+        error: body,
+        errorMessage: body.error,
+        statusCode: response.statusCode
+      })
+    } else {
+      const result = body.rate
+      body.result = result
+      callback(response.statusCode, {
+        jobRunID: input.id,
+        data: body,
+        result: result,
+        statusCode: response.statusCode
+      })
+    }
+  })
+}
 
 exports.gcpservice = (req, res) => {
-	createRequest(req.body, (statusCode, data) => {
-		res.status(statusCode).send(data);
-	});
-};
+  createRequest(req.body, (statusCode, data) => {
+    res.status(statusCode).send(data)
+  })
+}
 
 exports.handler = (event, context, callback) => {
-	createRequest(event, (statusCode, data) => {
-		callback(null, data);
-	});
-};
+  createRequest(event, (statusCode, data) => {
+    callback(null, data)
+  })
+}
 
 exports.handlerv2 = (event, context, callback) => {
-	createRequest(JSON.parse(event.body), (statusCode, data) => {
-		callback(null, {
-			statusCode: statusCode,
-			body: JSON.stringify(data),
-			isBase64Encoded: false
-		});
-	});
-};
+  createRequest(JSON.parse(event.body), (statusCode, data) => {
+    callback(null, {
+      statusCode: statusCode,
+      body: JSON.stringify(data),
+      isBase64Encoded: false
+    })
+  })
+}
 
-module.exports.createRequest = createRequest;
+module.exports.createRequest = createRequest
--- test/index_test.js
+++ test/index_test.js
@@ -1,67 +1,69 @@
-const assert = require('chai').assert;
-const createRequest = require('../index.js').createRequest;
+const assert = require('chai').assert
+const createRequest = require('../index.js').createRequest
 
 describe('createRequest', () => {
 
-	context('when requesting the price', () => {
-		const jobID = "278c97ffadb54a5bbb93cfec5f7b5503";
-		const req = {
-			id: jobID,
-			data: {
-				coin: "ETH",
-				market: "USD"
-			}
-		};
+  context('when requesting the price', () => {
+    const jobID = '278c97ffadb54a5bbb93cfec5f7b5503'
+    const req = {
+      id: jobID,
+      data: {
+        coin: 'ETH',
+        market: 'USD'
+      }
+    }
 
-		it('returns data to the node', (done) => {
-			createRequest(req, (statusCode, data) => {
-				assert.equal(statusCode, 200);
-				assert.equal(data.jobRunID, jobID);
-				assert.isNotEmpty(data.data);
-				assert.isNumber(data.result);
-				done();
-			});
-		});
-	});
+    it('returns data to the node', (done) => {
+      createRequest(req, (statusCode, data) => {
+        assert.equal(statusCode, 200)
+        assert.equal(data.jobRunID, jobID)
+        assert.isNotEmpty(data.data)
+        assert.isNumber(data.data.result)
+        assert.isNumber(data.result)
+        done()
+      })
+    })
+  })
 
-	context('when leaving coin and market unspecified', () => {
-		const jobID = "278c97ffadb54a5bbb93cfec5f7b5504";
-		const req = {
-			id: jobID,
-			data: {}
-		};
+  context('when leaving coin and market unspecified', () => {
+    const jobID = '278c97ffadb54a5bbb93cfec5f7b5504'
+    const req = {
+      id: jobID,
+      data: {}
+    }
 
-		it('defaults to ETH/USD', (done) => {
-			createRequest(req, (statusCode, data) => {
-				assert.equal(statusCode, 200);
-				assert.equal(data.jobRunID, jobID);
-				assert.isNotEmpty(data.data);
-				assert.isNumber(data.result);
-				assert.equal(data.data.asset_id_base, "ETH");
-				assert.equal(data.data.asset_id_quote, "USD");
-				done();
-			});
-		});
-	});
+    it('defaults to ETH/USD', (done) => {
+      createRequest(req, (statusCode, data) => {
+        assert.equal(statusCode, 200)
+        assert.equal(data.jobRunID, jobID)
+        assert.isNotEmpty(data.data)
+        assert.isNumber(data.data.result)
+        assert.isNumber(data.result)
+        assert.equal(data.data.asset_id_base, 'ETH')
+        assert.equal(data.data.asset_id_quote, 'USD')
+        done()
+      })
+    })
+  })
 
-	context('when a bad request is sent', t => {
-		const jobID = "278c97ffadb54a5bbb93cfec5f7b5505";
-		const req = {
-			id: jobID,
-			data: {
-				coin: "notreal",
-				market: "notreal"
-			}
-		};
+  context('when a bad request is sent', t => {
+    const jobID = '278c97ffadb54a5bbb93cfec5f7b5505'
+    const req = {
+      id: jobID,
+      data: {
+        coin: 'notreal',
+        market: 'notreal'
+      }
+    }
 
-		it('returns an error to the node', (done) => {
-			createRequest(req, (statusCode, data) => {
-				assert.isAbove(statusCode, 400);
-				assert.equal(data.jobRunID, jobID);
-				assert.isNotEmpty(data.error);
-				assert.isNotEmpty(data.errorMessage);
-				done();
-			});
-		});
-	});
-});
+    it('returns an error to the node', (done) => {
+      createRequest(req, (statusCode, data) => {
+        assert.isAbove(statusCode, 400)
+        assert.equal(data.jobRunID, jobID)
+        assert.isNotEmpty(data.error)
+        assert.isNotEmpty(data.errorMessage)
+        done()
+      })
+    })
+  })
+})
